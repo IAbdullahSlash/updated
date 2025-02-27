0b09aecf65233c529133ea9fddee9bd87270a654
IAbdullahSlash <144207221+IAbdullahSlash@users.noreply.github.com>
2025-02-27 14:48:21 +0530

    modified

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import * as readline from 'node:readline/promises';
 
-const API_KEY = "modified"; 
+const API_KEY = "AIzaSyCqc3zIUa59r7UvNbn7gy6rhRXbsZ6NcxE"; 
 
 const genAI = new GoogleGenerativeAI(API_KEY);
 
-
+async function runChat() {
+    const model = genAI.getGenerativeModel({
+        model: 'gemini-1.5-flash',
+        generationConfig: {
+            temperature: 0.25,
+            top_p: 0.95,
+            top_k: 40,
+            max_output_tokens: 8192,
+            response_mime_type: "text/plain",
+        },
+        safetySettings: [],
+        systemInstruction: "your name is SafeBrowse you are an AI agent who is responsible for detecting and recognizing harmful content, including misinformation, hate speech, and cyberbullying, from given data, and should highlight such content, provide corrections, and suggest appropriate actions accordingly, also you are made by the team 404 fixers, if data has been given with the intension not related to your main mission you should just explain why its not from your expertise, also tell about yourself only when asked.",
+    });
 
     let history = [];
 
