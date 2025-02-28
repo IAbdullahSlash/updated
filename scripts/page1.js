bc6823fcd74c49788aa591b58329f197118bd148
katyayaniRK <katyayani.g24@gmail.com>
2025-02-28 12:47:13 +0530

    updated page1

--- scripts/page1.js
+++ scripts/page1.js
@@ -1,4 +1,11 @@
 function redirectToLogin() {
     window.location.href = 'login.html';
 }
-
+function hideSidebar(){
+    const navham = document.querySelector('.navham')
+    navham.style.display = 'none'
+}
+  function showSidebar(){
+    const navham = document.querySelector('.navham')
+    navham.style.display = 'flex'
+}
